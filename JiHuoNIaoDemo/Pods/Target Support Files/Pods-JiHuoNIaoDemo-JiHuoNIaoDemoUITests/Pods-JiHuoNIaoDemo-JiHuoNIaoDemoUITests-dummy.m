c15cfacc6ecbe7ca00b79bf57419984eb9f724e8
刘群 <1271719322@qq.com>
2025-08-14 11:23:40 +0800

    no message

--- JiHuoNIaoDemo/Pods/Target Support Files/Pods-JiHuoNIaoDemo-JiHuoNIaoDemoUITests/Pods-JiHuoNIaoDemo-JiHuoNIaoDemoUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JiHuoNIaoDemo_JiHuoNIaoDemoUITests : NSObject
-@end
-@implementation PodsDummy_Pods_JiHuoNIaoDemo_JiHuoNIaoDemoUITests
-@end
--- JiHuoNIaoDemo/Pods/Target Support Files/Pods-JiHuoNIaoDemo/Pods-JiHuoNIaoDemo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_JiHuoNIaoDemo : NSObject
-@end
-@implementation PodsDummy_Pods_JiHuoNIaoDemo
-@end
